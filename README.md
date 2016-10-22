70b8929f71e49fff885d296fb86013935b420740
孙建华 <cumtsjh@163.com>
2016-10-22 10:29:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+网宿科技CDN SDK
+
+## 说明
+网宿科技CDN API参考手册（登录客户服务系统后可下载对应文档）：http://www.chinanetcenter.com/
+
+http 请求使用 Guzzle 三方库（v5.3），具体文档可参考：https://github.com/guzzle/guzzle
+
+## 安装
+```
+composer require eddy\chinanetcdn
+```
+
+## 使用方法
+```
+// 实例化客户端
+$client = new eddy\ChinaNetCDNClient('username', 'password');
+
+// 发送请求
+// 任务查询
+$response = $client->queryReceiver([
+    'starttime' => date('Y-m-d H:i:s', strtotime('-6 days')),//开始时间
+    'endtime' => date('Y-m-d H:i:s'),//结束时间
+    ]);
+$response = $client->contReceiver();
+
+// 更新
+$response = $client->contReceiver(
+    [
+        'url' => 'http://www.pc6.com/azyx/142394.html;http://www.pc6.com/azyx/203556.html',//url地址。多个url用分号相隔
+        'dir' => 'http://www.pc6.com/azyx/',//目录地址
+    ]
+);
+
+// http 请求返回内容
+echo $response->getBody();
+// http 响应状态码
+echo $response->getStatusCode();
+```
---
+++ src/ChinaNetCDNClient.php
@@ -0,0 +1,96 @@
+<?php
+namespace eddy;
+
+class ChinaNetCDNClient
+{
+    public $timeout = 10;
+
+    protected $username;
+    protected $password;
+
+    protected $url = 'http://ccm.chinanetcenter.com/ccm/servlet/';
+
+    /**
+     * @var \GuzzleHttp\Client Http Client
+     */
+    protected $httpClient;
+
+    function __construct($username = '', $password = '')
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->httpClient = new \GuzzleHttp\Client();
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (empty($arguments)) {
+            $origin_params = [];
+        } else {
+            if (isset($arguments[0]) && is_array($arguments[0])) {
+                $origin_params = $arguments[0];
+            } else {
+                throw new \InvalidArgumentException('the type of argument must be array.');
+            }
+        }
+        $origin_params['username'] = $this->username;
+        if ($name === 'contReceiver') {
+            $urls = '';
+            if (isset($origin_params['url'])) {
+                $urls = $origin_params['url'];
+            }
+            if (isset($origin_params['dir'])) {
+                $urls .= $origin_params['dir'];
+            }
+            if ($urls === '') {
+                throw new \InvalidArgumentException('missing url or dir parameter.');
+            }
+            $origin_params['passwd'] = md5($this->username . $this->password . $urls);
+        } else {
+            $origin_params['password'] = $this->getSign();
+        }
+
+        $query = $this->getQuery($origin_params);
+
+        try {
+            $response = $this->httpClient->get($this->url . $name . '?' . $query, ['timeout' => $this->timeout]);
+            return $response;
+        } catch (\GuzzleHttp\Exception\TransferException $e) {
+            $response = $e->getResponse();
+            return $response;
+        }
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
+    protected function getQuery($params, $encode = false)
+    {
+        if ($encode === true) {
+            $params = array_map('rawurlencode', $params);
+        }
+        return http_build_query($params, '', '&', PHP_QUERY_RFC3986);
+    }
+
+    protected function getSign()
+    {
+        return md5($this->username . $this->password);
+    }
+}
